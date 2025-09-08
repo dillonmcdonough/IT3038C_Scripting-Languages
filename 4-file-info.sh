8d4c732754559876dcf44bbbc62c8156826c1285
Dillon McDonough <dillon.mcdonough02@gmail.com>
2025-09-08 16:08:51 -0400

    re-comit of file as this version now passes test

--- 4-file-info.sh
+++ 4-file-info.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
 # Prompt the user to enter a file name/path
-echo "Please enter a file name or path:"
+if [ -t 0 ]; then
+  echo "Please enter a file name or path:"
+fi
 read file
 
-#Variables to store file information
-fsize=$(stat -c %s "$file")
-permissions=$(stat -c %A "$file")
-created=$(stat -c %w "$file")
-modified=$(stat -c %y "$file")
 
 # Check if the file exists
-if [ -e "$file" ];
-then
+if [ -e "$file" ]; then
+
+  # Variables to store file information (only if file exists)
+  fsize=$(stat -c %s "$file")
+  permissions=$(stat -c %A "$file")
+  created=$(stat -c %w "$file")
+  modified=$(stat -c %y "$file")
+
   # If the file exists, print the following information:
     echo "-----------------"
     echo "File Information:"
     echo "-----------------"
     echo "File: $file"
     echo "Size: $fsize bytes"
-    echo "Permissions: "
-    echo "Created: "
-    echo "Modified: "
+    echo "Permissions: $permissions"
+    echo "Created: $created"
+    echo "Modified: $modified"
     echo "-----------------"
 else
   # if the file doesn't exist, print an error message
